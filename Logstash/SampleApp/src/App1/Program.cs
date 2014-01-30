019b0e4f1e10fbc11e194f7c91b1c71f52039996
Matthew Smith <mtscout6@gmail.com>
2014-01-30 10:59:39 -0700

    Simple Tcp client server app

--- Logstash/SampleApp/src/App1/Program.cs
+++ Logstash/SampleApp/src/App1/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using log4net;
+using log4net.Config;
 
 namespace App1
 {
     class Program
     {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof (Program));
+
         static void Main(string[] args)
         {
+            XmlConfigurator.Configure();
+
+            var clientId = Guid.NewGuid();
+
+            var client = new TcpClient();
+            client.Connect(IPAddress.Parse("127.0.0.1"), 8001);
+
+            _logger.InfoFormat("Connected [Client: {0}]", clientId);
+
+            string line;
+
+            var streamWriter = new StreamWriter(client.GetStream());
+            streamWriter.WriteLine(clientId);
+
+            while (!String.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+                _logger.InfoFormat("Sending: {0} [Client: {1}]", line, clientId);
+                streamWriter.WriteLine(line);
+                streamWriter.Flush();
+            }
+
+            _logger.InfoFormat("Disconnecting [Client: {0}]", clientId);
         }
     }
 }
--- Logstash/SampleApp/src/App2/Program.cs
+++ Logstash/SampleApp/src/App2/Program.cs
@@ -1,15 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
 
 namespace App2
 {
     class Program
     {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof (Program));
+
         static void Main(string[] args)
         {
+            XmlConfigurator.Configure();
+
+            var ipAddress = IPAddress.Parse("127.0.0.1");
+
+            var listener = new TcpListener(ipAddress, 8001);
+
+            listener.Start();
+
+            _logger.Info("The server is running at port 8001");
+            _logger.InfoFormat("The local endpoint is: {0}", listener.LocalEndpoint);
+
+            var clientConnected = listener.AcceptTcpClientAsync();
+
+            ProcessConnectedClient(listener, clientConnected);
+
+            Console.WriteLine("Press enter to quit");
+            Console.ReadLine();
+        }
+
+        static void ProcessConnectedClient(TcpListener listener, Task<TcpClient> clientConnected)
+        {
+            clientConnected.ContinueWith(x =>
+            {
+                var client = x.Result;
+                var stream = client.GetStream();
+
+                _logger.InfoFormat("Connected Client: {0}", client.Client.LocalEndPoint);
+
+                ReadLine(Guid.Empty, new StreamReader(stream));
+
+                ProcessConnectedClient(listener, listener.AcceptTcpClientAsync());
+            });
+        }
+
+        static void ReadLine(Guid clientId, StreamReader stream)
+        {
+            stream.ReadLineAsync()
+                .ContinueWith(x =>
+                {
+                    var line = x.Result;
+
+                    if (clientId == Guid.Empty)
+                    {
+                        clientId = Guid.Parse(line);
+                    }
+                    else
+                    {
+                        _logger.InfoFormat("Received: {0} [From: {1}]", line, clientId);
+                    }
+
+                    if (stream.BaseStream.CanRead)
+                    {
+                        ReadLine(clientId, stream);
+                    }
+                });
         }
     }
 }
